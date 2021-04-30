7d8273634c5fe31e05fbc7084f3a85d2991ac204
Shashank C <shashank.c@valtech.com>
2021-04-30 18:26:34 +0530

    moved our directory and shop data into its own reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,30 +1,13 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
+import CollectionPage from '../collection/collection.component';
 
-import SHOP_DATA from './shop.data.js';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-
-class ShopPage extends React.Component {
-    constructor () {
-        super();
-
-        this.state = {
-            collections: SHOP_DATA
-        };
-
-    }
-
-    render() {
-        const {collections} = this.state; 
-        return (
-            <div className='shop-page'>
-                {
-                    collections.map(({id, ...othercollectionProps}) => (
-                        <CollectionPreview key={id} {...othercollectionProps}/>
-                    ))
-                }
-            </div>
-        );
-    }
-}
-
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+const ShopPage = ({match}) => (
+    <div className='shop-page'>
+        <Route exact path={`${match.path}`} component={CollectionsOverview}/>
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
+);
+  
 export default ShopPage;
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -21,7 +21,9 @@ export const removeItemFromCart = (cartItems, cartItemToRemove) => {
         return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
     }
 
-    return cartItems.map(
-        cartItem => cartItem.id === cartItem ? {...cartItem, quantity: cartItem.quantity -1 } : cartItem
+    return cartItems.map(cartItem => 
+        cartItem.id === cartItemToRemove.id
+        ? {...cartItem, quantity: cartItem.quantity - 1 } 
+        : cartItem
     );
-}
+};
